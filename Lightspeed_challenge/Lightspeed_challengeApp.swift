1a8c8d90b4509c368ea14a1c463c6b32581156c5
sheharyar-rr <sheharyarirfan_rc@hotmail.com>
2023-03-01 01:38:02 -0500

    Initial Commit

---
+++ Lightspeed_challenge/Lightspeed_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lightspeed_challengeApp.swift
+//  Lightspeed_challenge
+//
+//  Created by Sheharyar Irfan on 2023-03-01.
+//
+
+import SwiftUI
+
+@main
+struct Lightspeed_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
